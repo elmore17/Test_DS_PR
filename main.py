2d65d0bab15a28e1ebe620cf4c023a43339624a3
Danil Egorkin <93032618+elmore17@users.noreply.github.com>
2023-07-28 15:34:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import tensorflow as tf
 import os
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
